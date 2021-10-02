330c966785e039a13ddcb6e1b91c2908d627d7ee
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-02 11:20:51 +0530

    Update script.py

--- script.py
+++ script.py
@@ -13,28 +13,28 @@ Send me any video file and extract what you want :).
 
 See <i>help</i> for more details.
 
-© @TroJanzHEX</b>"""
+© @Tellybots_4u</b>"""
 
 
-    HELP_MSG = """<b>Hai, Follow these Steps..</b>
+    HELP_MSG = """<b>Hi, Follow these Steps..</b>
 
-🌀 <i>Send me any Valid video file.</i>
+ <i>Send me any Valid video file.</i>
 
-🌀 <i>Click Download and Process button to download the file to my server</i>
+ <i>Click Download and Process button to download the file to my server</i>
 
-🌀 <i>Wait till I process the video!</i>
+ <i>Wait till I process the video!</i>
 
-🌀 <i>Now select the stream you need to extract!</i>
+ <i>Now select the stream you need to extract!</i>
 
-<b>© @TroJanzHEX</b>"""
+<b>© @Tellybots_support</b>"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : Streams Extractor Bot</b>
+    ABOUT_MSG = """<b>My Name : Telly Audio Video Extractor Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+<b>Developer :</b> @Tellybots_4u    
 
-⭕️<b>Language :</b> <code>Python3</code>
+<b>Language :</b> <code>Python3</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
 """
