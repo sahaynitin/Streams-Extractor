e5d354f1940606f3a134ab5311cca3bb414fc448
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-02 11:22:50 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @trojanzhex
+
+
+class Script(object):
+
+    START_MSG = """<b>Hello {},
+
+I can extract audio and subtitles from video files.
+
+Send me any video file and extract what you want :).
+
+See <i>help</i> for more details.
+
+© @Tellybots_4u</b>"""
+
+
+    HELP_MSG = """<b>Hi, Follow these Steps..</b>
+
+ <i>Send me any Valid video file.</i>
+
+ <i>Click Download and Process button to download the file to my server</i>
+
+ <i>Wait till I process the video!</i>
+
+ <i>Now select the stream you need to extract!</i>
+
+<b>© @Tellybots_support</b>"""
+
+
+    ABOUT_MSG = """<b>My Name : Telly Audio Video Extractor Bot</b>
+
+<b>Developer :</b> @Tellybots_4u    
+
+<b>Language :</b> <code>Python3</code>
+
+<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+
+"""
