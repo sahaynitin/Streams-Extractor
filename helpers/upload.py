f12af6dfcb510f54f3b8da2d2d9d6316e2a6ef5d
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-02 11:28:05 +0530

    Update upload.py

--- helpers/upload.py
+++ helpers/upload.py
@@ -42,7 +42,7 @@ async def upload_audio(client, message, file_loc):
             chat_id=message.chat.id,
             audio=file_loc,
             thumb=thumb,
-            caption="**@TroJanzHEX**",
+            caption="**@Tellybots_4u**",
             title=title,
             performer=artist,
             duration=duration,
@@ -76,7 +76,7 @@ async def upload_subtitle(client, message, file_loc):
         await client.send_document(
             chat_id=message.chat.id,
             document=file_loc,
-            caption="**@TroJanzHEX**",
+            caption="**@Tellybots_4u**",
             progress=progress_func,
             progress_args=(
                 "**Uploading extracted subtitle...**",
